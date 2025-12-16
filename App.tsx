3e1ab9611f6850911be105b6fad1b9278dbc7597
tbuell124 <tybuell@gmail.com>
2025-12-16 10:56:47 -0500

    feat: streamline auto tuner ui

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StyleSheet, View } from 'react-native';
 
-import { NotificationSurface } from '@components/NotificationSurface';
-import { DebugLogOverlay } from '@components/DebugLogOverlay';
-import { SettingsModal } from '@components/SettingsModal';
 import { TunerScreen } from '@components/TunerScreen';
 import { TunerProvider } from '@state/TunerStateContext';
 import { NotificationProvider } from '@state/NotificationContext';
@@ -16,11 +12,7 @@ export default function App(): JSX.Element {
       <GestureHandlerRootView style={styles.root}>
         <TunerProvider>
           <View style={styles.container}>
-            <StatusBar style="light" />
             <TunerScreen />
-            <SettingsModal />
-            <NotificationSurface />
-            <DebugLogOverlay />
           </View>
         </TunerProvider>
       </GestureHandlerRootView>
